cbe1018d406cebaacce96c3445234e278188cbc2
Deep Patel <118447327+deepbajud@users.noreply.github.com>
2023-08-30 12:30:09 +0530

    Commit 4

---
+++ app/src/main/java/com/example/stringcomparemasterbajud/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.stringcomparemasterbajud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
